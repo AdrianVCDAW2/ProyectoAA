da73bc650820e17da85cc0a9261ad5042d54a54d
AdrianVCDAW2 <avitoricas1@educacion.navarra.es>
2023-01-25 12:34:58 +0100

    arreglo

--- vistas/cabecera.php
+++ vistas/cabecera.php
@@ -16,6 +16,12 @@
             if (isset($_SESSION['usuario'])) {
                 require "config/conectar.php";
 //                $sql = 'SELECT usuario FROM usuarios WHERE usuario="' . $_POST['usuario'] . '"';
+                $sql = 'SELECT id_usuario FROM usuarios WHERE usuario="' . $_SESSION['usuario'] . '"';
+                $resul = mysqli_query($conexion, $sql);
+                while ($id = mysqli_fetch_array($resul))
+                {
+                    $_SESSION['id'] = $id;
+                }
                 echo "<div class='right'><p class='inline'>Bienvenido, " . $_SESSION['usuario'] . "</p>";
                 echo "<img src='img/avatar.jpg' class='profile-pic'><a href='?opcion=gestionar_cuenta'><i class='fas fa-edit editar-arriba'></i></a>";
                 echo "<form method='POST' action='controlador/cerrar_sesion.php'><input class='header_input' type='submit' value='Cerrar sesión'/></form></div>";
