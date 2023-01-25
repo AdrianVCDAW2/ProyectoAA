95f15cd795b92d4abd042eb476f1e8a8d77496a5
alumno <aarrondov@gmail.com>
2023-01-25 13:55:27 +0100

    cambios varios

--- vistas/vista_inicio.php
+++ vistas/vista_inicio.php
@@ -1,9 +1,69 @@
 <?php
 
-echo "<table><tr><th>Usuario</th><th>Correo</th><th>Contraseña</th><th>Ruta</th></tr>";
-foreach ($usuarios as $u) {
-    echo "<tr><td>" . $u["usuario"] . "</td><td>" . $u["correo_usuario"] . "</td><td>" . $u["passwd_usuario"] . "</td><td><img class='ppic' src='uploads/profilepics/" . $u["perfil_usuario"] . "'></td></tr>";
-}
-echo "</table";
+//echo "<table><tr><th>Usuario</th><th>Correo</th><th>Contraseña</th><th>Ruta</th></tr>";
+//foreach ($usuarios as $u) {
+//    echo "<tr><td>" . $u["usuario"] . "</td><td>" . $u["correo_usuario"] . "</td><td>" . $u["passwd_usuario"] . "</td><td><img class='ppic' src='uploads/profilepics/" . $u["perfil_usuario"] . "'></td></tr>";
+//}
+//echo "</table";
+
+echo '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<div id="container">	
+	
+	<div class="product-details">
+		
+	<h1>HABITACIÓN INDIVIDUAL</h1>
+	<span class="hint-star star">
+		<i class="fa fa-star" aria-hidden="true"></i>
+		<i class="fa fa-star" aria-hidden="true"></i>
+		<i class="fa fa-star" aria-hidden="true"></i>
+		<i class="fa fa-star" aria-hidden="true"></i>
+		<i class="fa fa-star-half-o" aria-hidden="true"></i>
+	</span>
+		
+			<p class="information">" Lets spread the joy , here is Christmas , the most awaited day of the year.Christmas Tree is what one need the most. Here is the correct tree which will enhance your Christmas.</p>
+
+		
+		
+<div class="control">
+	<form id="comprar" method="get">
+	
+	<input type="hidden" name="opcion" value="crear_reserva">
+</form>
+	<button id="comprarBtn" class="btn">
+	 <span class="price">35.00€</span>
+   <span class="shopping-cart"><i class="fa fa-shopping-cart" aria-hidden="true"></i></span>
+   <span class="buy">Reservar</span>
+ </button>
+
+	
+</div>
+			
+</div>
+	
+<div class="product-image">
+	
+	<img src="https://www.casacelsa.com/wp-content/uploads/2016/05/habitacion2.jpg" alt="">
+	
+
+<div class="info">
+	<h2> Description</h2>
+	<ul>
+		<li><strong>Capacidad: </strong>1 persona</li>
+		<li><strong>Cama: </strong>1.90m * 0.90m</li>
+		<li><strong>Decoración: </strong>Contemporánea</li>
+		<li><strong>Vistas: </strong>Interior</li>
+		
+	</ul>
+</div>
+</div>
+
+</div>
+
+
+';
+echo "<script>document.getElementById('comprarBtn').addEventListener('click',function(){
+    document.getElementById('comprar').submit();
+})</script>";
+
 
 ?>
